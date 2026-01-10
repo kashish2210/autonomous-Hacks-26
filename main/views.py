02b5de19575dc28d7607a1e2fb5847679e7ffee7
kashish shrivastav <en.shrivastavkashish@gmail.com>
2026-01-11 03:59:11 +0530

    greater updates

--- main/views.py
+++ main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(request):
     return render(request, 'dashboard.html')
+def contact(request):
+    return render(request, 'contact.html')
+def privacy(request):
+    return render(request, 'privacy.html')
