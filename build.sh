5c72f2e808d208bf296492b770a44c4ec43f8c36
kashish shrivastav <en.shrivastavkashish@gmail.com>
2026-01-11 00:07:33 +0530

    Update app.yaml

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r pip-requirements
+
+python manage.py collectstatic --no-input
+python manage.py migrate
--- startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-gunicorn -b :$PORT --workers 2 --threads 4 credible.wsgi:application
